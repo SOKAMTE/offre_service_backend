675d5191f3d5f910ba903a4f551867714e74610e
YOUSSOUF SOKAMTE <youssoufngounou@gmail.com>
2020-08-26 05:56:46 +0100

    first commit

---
+++ src/main/java/com/app/OffreServiceApplication.java
@@ -0,0 +1,13 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OffreServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OffreServiceApplication.class, args);
+	}
+
+}
